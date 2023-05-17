cb7fba56d9d1555f7317f1c124cbe9eea1b36e34
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-17 17:52:05 +0530

    feat: center align content

--- src/app/image/layout.tsx
+++ src/app/image/layout.tsx
@@ -2,7 +2,7 @@ export default function ImageLayout({ children }: {
     children: React.ReactNode
 }) {
     return (
-        <div className="p-5">
+        <div className="p-5 w-screen h-screen flex items-center justify-center">
             {children}
         </div>
     )

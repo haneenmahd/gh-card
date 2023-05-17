c4a83b1b52920581ac5d9163eb9aa1f21e4f4fde
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-17 19:59:23 +0530

    feat: format numbers

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -40,36 +40,45 @@ const page = async ({ searchParams: { username, name } }: pageProps) => {
     return (
         <div className='w-fit mt-3 bg-slate-50 rounded-lg outline-none ring-1 ring-slate-300 overflow-clip shadow-sm'>
             <div className='p-5 bg-gradient-to-b from-white to-slate-50'>
-                <div className='flex flex-row'>
-                    <Image
-                        className='max-w-[50px] h-fit rounded-full mr-3'
-                        height={50}
-                        width={50}
-                        src={data.owner.avatar_url}
-                        alt={`Avatar for ${data.owner.login}`}
-                    />
-                    <div className="flex flex-col">
-                        <div className='mb-2'>
-                            <h3 className='text-2xl font-medium'>{data.name}</h3>
-                            <p className='text-slate-600'>{data.owner.login}</p>
+                <div className="flex flex-col">
+                    <div className='w-fit flex flex-row items-center justify-center p-1 pr-2 ring-1 ring-gray-200 rounded-2xl'>
+                        <Image
+                            className='max-w-[20px] h-fit rounded-full mr-2'
+                            height={20}
+                            width={20}
+                            src={data.owner.avatar_url}
+                            alt={`Avatar for ${data.owner.login}`}
+                        />
 
-                            <p className='my-3 text-slate-400'>{data.description}</p>
+                        <h3 className='text-sm font-medium'>{data.owner.login}</h3>
+                    </div>
+
+                    <div className='my-2'>
+                        <h3 className='text-2xl font-medium'>{data.name}</h3>
+                        <p className='mt-1 text-slate-400'>{data.description}</p>
+                    </div>
+
+                    <div className='flex flex-row gap-5'>
+                        <div>
+                            <p className='text-yellow-600'>
+                                {formatter(data.stargazers_count)} stars
+                            </p>
                         </div>
 
-                        <div className='flex flex-row gap-5'>
-                            <div>
-                                <p className='text-zinc-600'>{data.stargazers_count} stars</p>
-                            </div>
+                        <div>
+                            <p className='text-zinc-500'>
+                                {formatter(data.open_issues_count)} issues
+                            </p>
+                        </div>
 
-                            <div>
-                                <p className='text-zinc-500'>{data.open_issues_count} issues</p>
-                            </div>
-
-                            <div>
-                                <p className='text-zinc-400'>{data.forks_count} forks</p>
-                            </div>
+                        <div>
+                            <p className='text-zinc-400'>
+                                {formatter(data.forks_count)} forks
+                            </p>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
---
+++ src/lib/formatter.ts
@@ -0,0 +1,10 @@
+/**
+ * Format for thousands. If greater than thousand adds a "k" to the end to denote thousand.
+ */
+const formatter = (value: number): string => {
+    if (value > 1000) {
+        return `${(value / 1000).toFixed()}k`;
+    }
+
+    return `${value}`;
+}

6c3ba110bd48d50f03e685dc90355a34e1c00a36
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-17 19:19:01 +0530

    fix: loader

--- src/app/image/loading.tsx
+++ src/app/image/loading.tsx
@@ -1,8 +1,6 @@
 const loading = ({ }) => {
     return (
-        <div className='w-fit mt-3 bg-slate-50 rounded-lg outline-none ring-1 ring-slate-300 overflow-clip shadow-sm'>
-
-        </div>
+        <div className="animate-spin h-[20px] w-[20px] rounded-full border-2 border-gray-200 border-t-black" />
     )
 }
 

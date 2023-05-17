cdb21a88a7be2d5078adb21c47cb8c88a0235a5a
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-17 22:15:21 +0530

    fix: import `formatter`

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,3 +1,4 @@
+import { formatter } from '@/lib/formatter'
 import { Metadata } from 'next'
 import Image from 'next/image'
 
--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -1,7 +1,7 @@
 /**
  * Format for thousands. If greater than thousand adds a "k" to the end to denote thousand.
  */
-const formatter = (value: number): string => {
+export const formatter = (value: number): string => {
     if (value > 1000) {
         return `${(value / 1000).toFixed()}k`;
     }

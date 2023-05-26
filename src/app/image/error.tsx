f2c24eefd3c8f66f11a6a7ab349266875fee50b9
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-26 12:04:55 +0530

    feat: add error message with color red

--- src/app/image/error.tsx
+++ src/app/image/error.tsx
@@ -6,7 +6,7 @@ const error = ({ error, reset }: {
 }) => (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
         <p className="text-center">An error occured while generating image.</p>
-
+        <p className="text-center text-red-500">{error.message}</p>
         <button
             className='py-2 px-10 mt-5 w-max bg-black text-white ring-1 ring-black rounded-md hover:bg-white hover:text-black transition-colors'
             onClick={reset}>

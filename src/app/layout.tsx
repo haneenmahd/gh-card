34ac95a777d529ebf7bf010fe7d484132e62dab8
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-27 00:32:32 +0530

    remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: '(Card) - Quickly generate GitHub repository card',
   description: 'Generate and download github repository card in less than a second. Supaa fast!',
-  'google-site-verification': 'gMh6iXEuJLTxNWmsDP1PTyFmEdubu7mOHpImHz4_ZAI'
 }
 
 export default function RootLayout({

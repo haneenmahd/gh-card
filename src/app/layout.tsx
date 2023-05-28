8eb384d005456ac78fe4ab65a63fc136a1880b99
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-28 18:06:43 +0530

    fix: google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: '(Card) - Quickly generate GitHub repository card',
   description: 'Generate and download github repository card in less than a second. Supaa fast!',
+  other: {
+    'google-site-verification': 'gMh6iXEuJLTxNWmsDP1PTyFmEdubu7mOHpImHz4_ZAI',
+  }
 }
 
 export default function RootLayout({
@@ -19,7 +23,6 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <link rel='icon' href='/icon.jpeg' />
-        <meta name="google-site-verification" content="gMh6iXEuJLTxNWmsDP1PTyFmEdubu7mOHpImHz4_ZAI" />
       </Head>
       <body className={inter.className}>
         {children}

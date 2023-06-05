4283068062f4b855a329fe5ff42bea78b40886a1
Haneen Mahdin <haneenmahdin@gmail.com>
2023-06-05 15:07:04 +0530

    feat: add useRepo

--- src/hooks/useRepo.ts
+++ src/hooks/useRepo.ts
@@ -4,7 +4,7 @@ import { RepoData } from "@/lib/types";
 const fetcher = (...args: any) => fetch(args).then(res => res.json());
 
 export default function useRepo(username: string, repo: string) {
-    const response = useSWR<RepoData, Error>(`https://api.github.com/repos/${username}/${repo}`, fetcher)
+    const response = useSWR<RepoData, Error>(`https://api.github.com/repos/${username}/${repo}`, fetcher);
 
     return response;
 }

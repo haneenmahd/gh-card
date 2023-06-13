fa57ab94e4a4991ba08dd3d202397df74a556780
Haneen Mahdin <haneenmahdin@gmail.com>
2023-06-14 03:46:18 +0530

    feat: migrate to next.js pages router and fixed re-rendering issue

--- src/app/image/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function ImageLayout({ children }: {
-    children: React.ReactNode
-}) {
-    return (
-        <div className="p-5 w-screen h-screen flex flex-col items-center justify-center">
-            {children}
-        </div>
-    )
-}

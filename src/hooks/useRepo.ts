19a5fc884269f0fcc6166a393a0d7b2290d4b57e
Haneen Mahdin <haneenmahdin@gmail.com>
2023-06-26 20:04:24 +0530

    feat: update type reference for repo data

--- src/hooks/useRepo.ts
+++ src/hooks/useRepo.ts
@@ -1,10 +1,10 @@
 import useSWR from "swr";
-import { RepoData } from "@/lib/types";
+import { Repo } from "@/lib/types";
 
 const fetcher = (...args: any) => fetch(args).then(res => res.json());
 
 export default function useRepo(username: string, repo: string) {
-    const response = useSWR<RepoData, Error>(`https://api.github.com/repos/${username}/${repo}`, fetcher);
+    const response = useSWR<Repo, Error>(`https://api.github.com/repos/${username}/${repo}`, fetcher);
 
     return response;
 }

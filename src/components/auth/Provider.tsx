f2010e6ed38a350b8404abdb94b5cb83babb43bf
Haneen Mahdin <haneenmahdin@gmail.com>
2023-07-03 01:01:24 +0530

    chore: remove older packages

--- src/components/auth/Provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { SessionProvider } from 'next-auth/react';
-import { FC, ReactNode } from 'react';
-
-const Provider: FC<{ children: ReactNode; }> = ({ children }) =>
-    <SessionProvider>{children}</SessionProvider>;
-
-export default Provider;
